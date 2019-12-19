1b64640c5e935da56f973603cc6089aabbff445a
Povetkin Dmitriy <povdima19@gmail.com>
2019-12-19 15:40:22 +0200

    Updates

---
+++ precache-manifest.bded0fa2948ce2e0deb8b869a96a5661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c018db3b1808e9904ade162460c92936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e86dae5a2bb02458c03",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b40e8a60d5986b1b47b2",
+    "url": "/static/js/2.f04609b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f04609b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e86dae5a2bb02458c03",
+    "url": "/static/js/main.6a5286d0.chunk.js"
+  },
+  {
+    "revision": "3213ed711a3ec7403488",
+    "url": "/static/js/runtime-main.76e7e3d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
