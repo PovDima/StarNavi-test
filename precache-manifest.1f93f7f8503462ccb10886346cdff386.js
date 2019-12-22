6967ddeb79d21ef8e22754842b6a42741d20c7c1
Povetkin Dmitriy <povdima19@gmail.com>
2019-12-22 18:03:16 +0200

    Updates

---
+++ precache-manifest.1f93f7f8503462ccb10886346cdff386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4a95bea4858d2b8f8d7a31bf8343f5",
+    "url": "/StarNavi-test/index.html"
+  },
+  {
+    "revision": "8fad377d9e15bd91bf3c",
+    "url": "/StarNavi-test/static/css/main.3e6c199b.chunk.css"
+  },
+  {
+    "revision": "2b49265114e0b8421bfd",
+    "url": "/StarNavi-test/static/js/2.94b78ea9.chunk.js"
+  },
+  {
+    "revision": "409e055959155424c6f2015fe96dd220",
+    "url": "/StarNavi-test/static/js/2.94b78ea9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fad377d9e15bd91bf3c",
+    "url": "/StarNavi-test/static/js/main.51f38932.chunk.js"
+  },
+  {
+    "revision": "c08bc0637091bc5b2236",
+    "url": "/StarNavi-test/static/js/runtime-main.f1364443.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StarNavi-test/precache-manifest.888d281d0a041a0597671b8e766b15ef.js"
+  "/StarNavi-test/precache-manifest.1f93f7f8503462ccb10886346cdff386.js"
 );
 
 self.addEventListener('message', (event) => {
